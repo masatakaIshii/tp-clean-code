28691c87590bd5df47b899c3931f430d8906b691
MasatakaIshii <masaradio78@gmail.com>
2021-02-09 18:45:52 +0100

    ReturnBorrowedBook tested

--- core/src/main/java/fr/esgi/masa/tpcleancode/core/use_case/ReturnBorrowedBook.java
+++ core/src/main/java/fr/esgi/masa/tpcleancode/core/use_case/ReturnBorrowedBook.java
@@ -3,13 +3,21 @@ package fr.esgi.masa.tpcleancode.core.use_case;
 import fr.esgi.masa.tpcleancode.core.entity.Book;
 import fr.esgi.masa.tpcleancode.core.entity.BorrowedBook;
 import fr.esgi.masa.tpcleancode.core.entity.User;
+import fr.esgi.masa.tpcleancode.core.parser.IncorrectContentException;
 import fr.esgi.masa.tpcleancode.core.storage.Storage;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ReturnBorrowedBook implements LibraryAction {
-    public ReturnBorrowedBook(Storage<Book> booksStorage, Storage<BorrowedBook> borrowBooksStorage, Storage<User> userStorage) {
+    private final Storage<Book> bookStorage;
+    private final Storage<BorrowedBook> borrowedBookStorage;
+    private final Storage<User> userStorage;
 
+    public ReturnBorrowedBook(Storage<Book> booksStorage, Storage<BorrowedBook> borrowBooksStorage, Storage<User> userStorage) {
+        this.bookStorage = booksStorage;
+        this.borrowedBookStorage = borrowBooksStorage;
+        this.userStorage = userStorage;
     }
 
     @Override
@@ -18,7 +26,27 @@ public class ReturnBorrowedBook implements LibraryAction {
     }
 
     @Override
-    public void execute(List<String> arguments) {
+    public void execute(List<String> arguments) throws Exception {
+        if (arguments.size() != 3) throw new IllegalArgumentException();
 
+        var userLogin = arguments.get(1);
+        var borrowedBookTitle = arguments.get(2);
+        var listBorrowedBook = borrowedBookStorage.getAll();
+
+        var foundBorrowedBook = searchBorrowedBook(userLogin, borrowedBookTitle, listBorrowedBook);
+        bookStorage.add(foundBorrowedBook.getBook());
+        borrowedBookStorage.remove(foundBorrowedBook);
+    }
+
+    private BorrowedBook searchBorrowedBook(String userLogin, String borrowedBookTitle, List<BorrowedBook> listBorrowedBook) throws NotAuthorizedException {
+        var foundBorrowedBook = listBorrowedBook.stream()
+                .filter(borrowedBook -> {
+                    return borrowedBook.getUser().getLogin().equals(userLogin)
+                            && borrowedBook.getBook().getTitle().equals(borrowedBookTitle);
+                })
+                .findFirst()
+                .orElse(null);
+        if (foundBorrowedBook == null) throw new NotAuthorizedException("Not borrowed book is present");
+        return foundBorrowedBook;
     }
 }
---
+++ core/src/test/java/fr/esgi/masa/tpcleancode/core/use_case/ReturnBorrowedBookTest.java
@@ -0,0 +1,123 @@
+package fr.esgi.masa.tpcleancode.core.use_case;
+
+import fr.esgi.masa.tpcleancode.core.entity.Book;
+import fr.esgi.masa.tpcleancode.core.entity.BorrowedBook;
+import fr.esgi.masa.tpcleancode.core.entity.User;
+import fr.esgi.masa.tpcleancode.core.entity.UserRole;
+import fr.esgi.masa.tpcleancode.core.parser.IncorrectContentException;
+import fr.esgi.masa.tpcleancode.core.storage.Storage;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReturnBorrowedBookTest {
+    ReturnBorrowedBook sut;
+
+    @Mock
+    Storage<Book> mockBookStorage;
+
+    @Mock
+    Storage<BorrowedBook> mockBorrowedBookStorage;
+
+    @Mock
+    Storage<User> mockUserStorage;
+
+    @Before
+    public void setUp() {
+        sut = new ReturnBorrowedBook(mockBookStorage, mockBorrowedBookStorage, mockUserStorage);
+
+    }
+
+    @Test
+    public void actionNameIsReturnBorrowedBook() {
+        Assertions.assertThat(sut.actionName()).isEqualTo("returnBorrowedBook");
+    }
+
+    @Test
+    public void whenArgumentsNotEnough_shouldThrowException() {
+        List<String> arguments = new ArrayList<>();
+        arguments.add("returnBorrowedBook");
+        arguments.add("Gertrude");
+
+        Assertions.assertThatThrownBy(() -> {
+            sut.execute(arguments);
+        })
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenBorrowedBookOnArgumentNoPresent_shouldThrowException() throws IOException, IncorrectContentException {
+        List<String> arguments = new ArrayList<>();
+        arguments.add("returnBorrowedBook");
+        arguments.add("Gertrude");
+        arguments.add("TitleBorrowedBook");
+
+        var borrowedBook = new BorrowedBook(
+                new Book("title1", "authorName1", "oijoijo"),
+                new User("test", UserRole.MEMBER),
+                LocalDate.now()
+        );
+        var borrowedList = new ArrayList<BorrowedBook>();
+        borrowedList.add(borrowedBook);
+        Mockito.when(mockBorrowedBookStorage.getAll()).thenReturn(borrowedList);
+
+        Assertions.assertThatThrownBy(() -> sut.execute(arguments))
+                .isExactlyInstanceOf(NotAuthorizedException.class)
+                .hasMessage("Not borrowed book is present");
+    }
+
+    @Test
+    public void whenDataCorrect_shouldAddBookStorage() throws Exception {
+        List<String> arguments = new ArrayList<>();
+        arguments.add("returnBorrowedBook");
+        arguments.add("Gertrude");
+        arguments.add("TitleBorrowedBook");
+        var book = new Book("TitleBorrowedBook", "authorName1", "oijoijo");
+        var user = new User("Gertrude", UserRole.MEMBER);
+        var borrowedBook = new BorrowedBook(
+                book,
+                user,
+                LocalDate.now()
+        );
+        var borrowedList = new ArrayList<BorrowedBook>();
+        borrowedList.add(borrowedBook);
+        Mockito.when(mockBorrowedBookStorage.getAll()).thenReturn(borrowedList);
+
+        sut.execute(arguments);
+
+        Mockito.verify(mockBookStorage).add(book);
+    }
+
+    @Test
+    public void whenDataCorrect_shouldRemoveBorrowedBook() throws Exception {
+        List<String> arguments = new ArrayList<>();
+        arguments.add("returnBorrowedBook");
+        arguments.add("Gertrude");
+        arguments.add("TitleBorrowedBook");
+        var book = new Book("TitleBorrowedBook", "authorName1", "oijoijo");
+        var user = new User("Gertrude", UserRole.MEMBER);
+        var borrowedBook = new BorrowedBook(
+                book,
+                user,
+                LocalDate.now()
+        );
+        var borrowedList = new ArrayList<BorrowedBook>();
+        borrowedList.add(borrowedBook);
+        Mockito.when(mockBorrowedBookStorage.getAll()).thenReturn(borrowedList);
+
+        sut.execute(arguments);
+
+        Mockito.verify(mockBorrowedBookStorage).remove(borrowedBook);
+    }
+}
